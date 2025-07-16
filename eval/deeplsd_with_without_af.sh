ad7852372dfff167cbab42c88541b9bbad288d18
Francois Costa <frankost.costa@gmail.com>
2025-07-16 09:23:49 +0200

    Enable support to run evaluation on Euler and angle field comparaison with faster_lsd
    (cherry picked from commit 6a7b41c503a87b1845845e8bb21284046450c217)

--- eval/deeplsd_with_without_af.sh
+++ eval/deeplsd_with_without_af.sh
@@ -1,3 +1,28 @@
+#!/bin/bash
+# Cmd params 'run_training_euler.sh [exp_name] [path to conf]'
+
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --gpus=4
+#SBATCH --gres=gpumem:23g
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=6000
+
+if [[ "$(hostname)" == eu* ]]; then
+  module load eth_proxy
+
+  module load stack/2024-06
+  module load python_cuda/3.11.6
+  module load cmake/3.27.7
+  module load eigen/3.4.0
+  module load ceres-solver/2.2.0
+  module load glog/0.6.0-sx7hlp6
+  module load gflags/2.2.2-gpd4lxs
+
+  source /cluster/home/fcosta/myenv/bin/activate
+fi
+
 mkdir eval/af_evaluation
 
 echo "Evaluation of deeplsd without angle field"
@@ -9,3 +34,13 @@ echo "Evaluation of deeplsd with angle field"
 
 python -m gluefactory.eval.hpatches_lines --conf gluefactory/configs/eval/deeplsd+AF+LM.yaml --overwrite > "eval/af_evaluation/hpatches_deeplsd_with_af.txt"
 python -m gluefactory.eval.rdnim_lines --conf gluefactory/configs/eval/deeplsd+AF+LM.yaml --overwrite > "eval/af_evaluation/rdnim_lines_deeplsd_with_af.txt"
+
+echo "Evaluation of deeplsd without angle field - with fast_lsd"
+
+python -m gluefactory.eval.hpatches_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+LM.yaml --overwrite > "eval/af_evaluation/hpatches_deeplsd+fastlsd_without_af.txt"
+python -m gluefactory.eval.rdnim_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+LM.yaml --overwrite > "eval/af_evaluation/rdnim_lines_deeplsd+fastlsd_without_af.txt"
+
+echo "Evaluation of deeplsd with angle field - with fast_lsd"
+
+python -m gluefactory.eval.hpatches_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+AF+LM.yaml --overwrite > "eval/af_evaluation/hpatches_deeplsd+fastlsd_with_af.txt"
+python -m gluefactory.eval.rdnim_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+AF+LM.yaml --overwrite > "eval/af_evaluation/rdnim_lines_deeplsd+fastlsd_with_af.txt"
--- eval/lsd_vs_faster.sh
+++ eval/lsd_vs_faster.sh
@@ -1,3 +1,29 @@
+#!/bin/bash
+# Cmd params 'run_training_euler.sh [exp_name] [path to conf]'
+
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --gpus=4
+#SBATCH --gres=gpumem:23g
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=6000
+
+if [[ "$(hostname)" == eu* ]]; then
+  module load eth_proxy
+
+  module load stack/2024-06
+  module load python_cuda/3.11.6
+  module load cmake/3.27.7
+  module load eigen/3.4.0
+  module load ceres-solver/2.2.0
+  module load glog/0.6.0-sx7hlp6
+  module load gflags/2.2.2-gpd4lxs
+
+  source /cluster/home/fcosta/myenv/bin/activate
+fi
+
+
 mkdir eval/fast_lsd_eval
 
 echo "Evaluation on HPatches Dataset - raw lsd vs fast lsd"
