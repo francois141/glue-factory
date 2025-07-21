0b8080d4bb5b929774e37f59aa6276752ee492cc
François Costa <frankost.costa@gmail.com>
2025-07-21 17:33:30 +0200

    Speedup evaluation by running it in parallel

--- gluefactory/eval/hpatches_lines.py
+++ gluefactory/eval/hpatches_lines.py
@@ -32,6 +32,13 @@ from gluefactory.utils.export_predictions import export_predictions
 from gluefactory.utils.tensor import map_tensor
 from gluefactory.visualization.viz2d import plot_images, plot_lines, save_plot
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from collections import defaultdict
+from tqdm import tqdm
+import numpy as np
+import torch
+import os
+import matplotlib.pyplot as plt
 
 class HPatchesPipeline(EvalPipeline):
     default_conf = {
@@ -142,34 +149,24 @@ class HPatchesPipeline(EvalPipeline):
         s, r = load_eval(experiment_dir)
         return s, f, r
 
-    def run_eval(
-        self, loader: torch.utils.data.DataLoader, pred_file: Path, plot: bool
-    ):
+
+    def run_eval(self, loader: torch.utils.data.DataLoader, pred_file: Path, plot: bool):
         assert pred_file.exists()
         results = defaultdict(list)
-
         cache_loader = CacheLoader({"path": str(pred_file), "collate": None}).eval()
-        for i, data in enumerate(tqdm(loader)):
-            # if i in range(360,365):
-            #     continue
+
+        def evaluate_sample(i, data):
             pred = cache_loader(data)
-            # Remove batch dimension
             data = map_tensor(data, lambda t: torch.squeeze(t, dim=0))
-            # add custom evaluations here
+            results_i = {
+                "names": data["name"][0],
+                "scenes": data["scene"][0],
+            }
 
-            results_i = {}
-
-            # we also store the names for later reference
-            results_i["names"] = data["name"][0]
-            results_i["scenes"] = data["scene"][0]
-
-            # compute H_err
-            segs1, segs2, matched_idx1, matched_idx2 = (
-                pred["lines0"],
-                pred["lines1"],
-                pred["line_matches0"].to(torch.int64),
-                pred["line_matches1"].to(torch.int64),
-            )
+            # Compute H_err
+            segs1, segs2 = pred["lines0"], pred["lines1"]
+            matched_idx1 = pred["line_matches0"].to(torch.int64)
+            matched_idx2 = pred["line_matches1"].to(torch.int64)
 
             H = data["H_0to1"].cpu().numpy()
 
@@ -187,7 +184,7 @@ class HPatchesPipeline(EvalPipeline):
                         reproj_thresh=thresh,
                     )[0]
 
-            # compute repeatability and loc_error
+            # Repeatability and localization error
             if "lines0" in pred:
                 lines0 = pred["lines0"].cpu()
                 lines1 = pred["lines1"].cpu()
@@ -218,32 +215,41 @@ class HPatchesPipeline(EvalPipeline):
                 )
                 results_i["num_lines"] = (lines0.shape[0] + lines1.shape[0]) / 2
 
-            for k, v in results_i.items():
-                results[k].append(v)
+            return results_i
 
-        # summarize results as a dict[str, float]
-        # you can also add your custom evaluations here
+        # Run in parallel with a progress bar
+        with ThreadPoolExecutor() as executor:
+            futures = {
+                executor.submit(evaluate_sample, i, data): i
+                for i, data in enumerate(loader)
+            }
+            for future in tqdm(as_completed(futures), total=len(futures), desc="Evaluating"):
+                results_i = future.result()
+                for k, v in results_i.items():
+                    results[k].append(v)
+
+        # Summarize results
         summaries = {}
         for k, v in results.items():
             arr = np.array(v)
-            if not np.issubdtype(np.array(v).dtype, np.number):
+            if not np.issubdtype(arr.dtype, np.number):
                 continue
             if k.startswith("H_err"):
                 summaries[f"m{k}"] = round(np.mean(arr), 3)
             else:
                 summaries[f"m{k}"] = round(np.median(arr), 3)
 
-        if "repeatability" in results.keys():
+        if "repeatability" in results:
             for i, th in enumerate(self.conf.repeatability_th):
-                cur_nums = list(map(lambda x: x[i], results["repeatability"]))
-                summaries[f"repeatability@{th}px"] = round(np.median(cur_nums), 3)
-        if "loc_error" in results.keys():
+                values = [x[i] for x in results["repeatability"]]
+                summaries[f"repeatability@{th}px"] = round(np.median(values), 3)
+
+        if "loc_error" in results:
             for i, th in enumerate(self.conf.num_lines_th):
-                cur_nums = list(map(lambda x: x[i], results["loc_error"]))
-                summaries[f"loc_error@{th}lines"] = round(np.median(cur_nums), 3)
+                values = [x[i] for x in results["loc_error"]]
+                summaries[f"loc_error@{th}lines"] = round(np.median(values), 3)
 
         figures = {}
-
         return summaries, figures, results
 
 
--- gluefactory/eval/rdnim_lines.py
+++ gluefactory/eval/rdnim_lines.py
@@ -106,32 +106,23 @@ class RDNIMPipeline(EvalPipeline):
             )
         return pred_file
 
-    def run_eval(
-        self, loader: torch.utils.data.DataLoader, pred_file: Path
-    ):
+    def run_eval(self, loader: torch.utils.data.DataLoader, pred_file: Path, plot: bool):
         assert pred_file.exists()
         results = defaultdict(list)
-
         cache_loader = CacheLoader({"path": str(pred_file), "collate": None}).eval()
-        for i, data in enumerate(tqdm(loader)):
+
+        def evaluate_sample(i, data):
             pred = cache_loader(data)
-            # Remove batch dimension
             data = map_tensor(data, lambda t: torch.squeeze(t, dim=0))
-            # add custom evaluations here
+            results_i = {
+                "names": data["name"][0],
+                "scenes": data["scene"][0],
+            }
 
-            results_i = {}
-
-            # we also store the names for later reference
-            results_i["names"] = data["name"][0]
-            results_i["scenes"] = data["scene"][0]
-
-            # compute H_err
-            segs1, segs2, matched_idx1, matched_idx2 = (
-                pred["lines0"],
-                pred["lines1"],
-                pred["line_matches0"].to(torch.int64),
-                pred["line_matches1"].to(torch.int64),
-            )
+            # Compute H_err
+            segs1, segs2 = pred["lines0"], pred["lines1"]
+            matched_idx1 = pred["line_matches0"].to(torch.int64)
+            matched_idx2 = pred["line_matches1"].to(torch.int64)
 
             H = data["H_0to1"].cpu().numpy()
 
@@ -149,10 +140,23 @@ class RDNIMPipeline(EvalPipeline):
                         reproj_thresh=thresh,
                     )[0]
 
-            # compute repeatability and loc_error
+            # Repeatability and localization error
             if "lines0" in pred:
                 lines0 = pred["lines0"].cpu()
                 lines1 = pred["lines1"].cpu()
+
+                if plot:
+                    plot_images(
+                        [
+                            data["view0"]["image"].permute(1, 2, 0),
+                            data["view1"]["image"].permute(1, 2, 0),
+                        ],
+                        ["H0", "H1"],
+                    )
+                    plot_lines(lines=[pred["orig_lines0"], pred["orig_lines1"]])
+                    save_plot(os.path.join("./match_score/", f"{i}.jpg"))
+                    plt.close()
+
                 results_i["repeatability"] = compute_repeatability(
                     lines0,
                     lines1,
@@ -167,32 +171,41 @@ class RDNIMPipeline(EvalPipeline):
                 )
                 results_i["num_lines"] = (lines0.shape[0] + lines1.shape[0]) / 2
 
-            for k, v in results_i.items():
-                results[k].append(v)
+            return results_i
 
-        # summarize results as a dict[str, float]
-        # you can also add your custom evaluations here
+        # Run in parallel with a progress bar
+        with ThreadPoolExecutor() as executor:
+            futures = {
+                executor.submit(evaluate_sample, i, data): i
+                for i, data in enumerate(loader)
+            }
+            for future in tqdm(as_completed(futures), total=len(futures), desc="Evaluating"):
+                results_i = future.result()
+                for k, v in results_i.items():
+                    results[k].append(v)
+
+        # Summarize results
         summaries = {}
         for k, v in results.items():
             arr = np.array(v)
-            if not np.issubdtype(np.array(v).dtype, np.number):
+            if not np.issubdtype(arr.dtype, np.number):
                 continue
             if k.startswith("H_err"):
                 summaries[f"m{k}"] = round(np.mean(arr), 3)
             else:
                 summaries[f"m{k}"] = round(np.median(arr), 3)
 
-        if "repeatability" in results.keys():
+        if "repeatability" in results:
             for i, th in enumerate(self.conf.repeatability_th):
-                cur_nums = list(map(lambda x: x[i], results["repeatability"]))
-                summaries[f"repeatability@{th}px"] = round(np.median(cur_nums), 3)
-        if "loc_error" in results.keys():
+                values = [x[i] for x in results["repeatability"]]
+                summaries[f"repeatability@{th}px"] = round(np.median(values), 3)
+
+        if "loc_error" in results:
             for i, th in enumerate(self.conf.num_lines_th):
-                cur_nums = list(map(lambda x: x[i], results["loc_error"]))
-                summaries[f"loc_error@{th}lines"] = round(np.median(cur_nums), 3)
+                values = [x[i] for x in results["loc_error"]]
+                summaries[f"loc_error@{th}lines"] = round(np.median(values), 3)
 
         figures = {}
-
         return summaries, figures, results
 
 
