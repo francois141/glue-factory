efe85238eb339db498c847f46b93fd772eec3e1d
François Costa <frankost.costa@gmail.com>
2025-07-25 11:27:35 +0200

    Refactor experiment infrastructure

---
+++ jpl_scripts/eval_deeplsd_with_without_af.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Cmd params 'run_training_euler.sh [exp_name] [path to conf]'
+
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --gpus=4
+#SBATCH --gres=gpumem:23g
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=6000
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+source $DIR/common.sh
+
+SetupStack
+
+mkdir $DIR/eval/
+mkdir $DIR/eval/af_evaluation
+
+OUTPUT_DIR=$DIR/eval/af_evaluation
+
+echo "Evaluation of deeplsd without angle field"
+
+python -m gluefactory.eval.hpatches_lines --conf gluefactory/configs/eval/deeplsd+LM.yaml --overwrite > "${OUTPUT_DIR}/hpatches_deeplsd_without_af.txt"
+python -m gluefactory.eval.rdnim_lines --conf gluefactory/configs/eval/deeplsd+LM.yaml --overwrite > "${OUTPUT_DIR}/rdnim_lines_deeplsd_without_af.txt"
+
+echo "Evaluation of deeplsd with angle field"
+
+python -m gluefactory.eval.hpatches_lines --conf gluefactory/configs/eval/deeplsd+AF+LM.yaml --overwrite > "${OUTPUT_DIR}/hpatches_deeplsd_with_af.txt"
+python -m gluefactory.eval.rdnim_lines --conf gluefactory/configs/eval/deeplsd+AF+LM.yaml --overwrite > "${OUTPUT_DIR}/rdnim_lines_deeplsd_with_af.txt"
+
+echo "Evaluation of deeplsd without angle field - with fast_lsd"
+
+python -m gluefactory.eval.hpatches_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+LM.yaml --overwrite > "${OUTPUT_DIR}/hpatches_deeplsd+fastlsd_without_af.txt"
+python -m gluefactory.eval.rdnim_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+LM.yaml --overwrite > "${OUTPUT_DIR}/rdnim_lines_deeplsd+fastlsd_without_af.txt"
+
+echo "Evaluation of deeplsd with angle field - with fast_lsd"
+
+python -m gluefactory.eval.hpatches_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+AF+LM.yaml --overwrite > "${OUTPUT_DIR}/hpatches_deeplsd+fastlsd_with_af.txt"
+python -m gluefactory.eval.rdnim_lines --conf gluefactory/configs/eval/deeplsd+fastlsd+AF+LM.yaml --overwrite > "${OUTPUT_DIR}/rdnim_lines_deeplsd+fastlsd_with_af.txt"
--- jpl_scripts/oxparis100k_euler_worker.sh
+++ jpl_scripts/oxparis100k_euler_worker.sh
@@ -9,6 +9,9 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=6000
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+source $DIR/common.sh
+
 CHUNK_VALUE=$1
 
 if [ -z "$CHUNK_VALUE" ]; then
@@ -16,17 +19,7 @@ if [ -z "$CHUNK_VALUE" ]; then
   exit 1
 fi
 
-module load eth_proxy
-
-module load stack/2024-06
-module load python_cuda/3.11.6
-module load cmake/3.27.7
-module load eigen/3.4.0
-module load ceres-solver/2.2.0
-module load glog/0.6.0-sx7hlp6
-module load gflags/2.2.2-gpd4lxs
-
-source /cluster/home/fcosta/myenv/bin/activate
+SetupStack
 
 echo "Running chunk $CHUNK_VALUE"
 python -m gluefactory.ground_truth_generation.oxparis_100k oxford_paris_mini_100k --num_H 100 --n_jobs 2 --n_jobs_dataloader 2 --chunk $CHUNK_VALUE
