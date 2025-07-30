3b51941fc0f7ce3e81fee0a72f87775b9dd5fca0
François Costa <frankost.costa@gmail.com>
2025-07-30 10:47:34 +0200

    Add script to fetch_remove_values from cluster and improve jpl visualisation output

---
+++ fetch_cluster_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Transfering training outputs on the local computer"
+
+# Replace with your ETHZ username
+REMOTE_USER="fcosta"       
+REMOTE_HOST="euler.ethz.ch"
+REMOTE_PATH="/cluster/scratch/$REMOTE_USER/outputs/"
+LOCAL_PATH="./outputs/"
+
+rsync -avz --progress "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}" "${LOCAL_PATH}"
+
+echo "Transfering the dataset - abort script if you don't need this part"
+
+REMOTE_DATASET=oxparis_100k
+REMOTE_PATH_DATASET="/cluster/scratch/fcosta/outputs/results/$REMOTE_DATASET"
+LOCAL_PATH_DATASET="./data/$REMOTE_DATASET/"
+
+rsync -avz --progress "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH_DATASET}" "${LOCAL_PATH_DATASET}"
--- gluefactory/test_oxparis_100k_output.py
+++ gluefactory/test_oxparis_100k_output.py
@@ -6,84 +6,78 @@ Author: Paul-Edouard Sarlin (skydes)
 
 import argparse
 import copy
-import re
-import shutil
-import signal
 from collections import defaultdict
 from pathlib import Path
 from pydoc import locate
 
 import numpy as np
-import torch
 from omegaconf import OmegaConf
-from torch.cuda.amp import GradScaler, autocast
-from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
+from gluefactory.visualization.viz2d import plot_images, plot_lines
+import cv2
 
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.ndimage import binary_dilation
-import flow_vis
 
-from gluefactory.eval.io import get_eval_parser, load_model, parse_eval_args
+from gluefactory.eval.io import load_model
 
 from . import __module_name__, logger
 from .datasets import get_dataset
-from .eval import run_benchmark
-from .models import get_model
-from .settings import EVAL_PATH, TRAINING_PATH
-from .utils.experiments import get_best_checkpoint, get_last_checkpoint, save_experiment
-from .utils.stdout_capturing import capture_outputs
-from .utils.tensor import batch_to_device
-from .utils.tools import (
-    AverageMetric,
-    MedianMetric,
-    PRMetric,
-    RecallMetric,
-    fork_rng,
-    set_seed,
-)
 
 def nicer_display(df):
     return (df) ** (1/4)
 
-def get_flow_vis(df, ang, line_neighborhood=5):
-    norm = line_neighborhood + 1 - np.clip(df, 0, line_neighborhood)
-    flow_uv = np.stack([norm * np.cos(ang), norm * np.sin(ang)], axis=-1)
-    flow_img = flow_vis.flow_to_color(flow_uv, convert_to_bgr=False)
-    return flow_img
-
-def visualize_img_with_gt(name, dset, num=5, offset=0, lim_kpoints=-1):
+def visualize_img_with_gt(name, elem, num=5, offset=0, lim_kpoints=-1):
     plt.figure()
-    idxs = list(range(offset, offset+num, 1))
-    _, ax = plt.subplots(1, 3, figsize=(20, 8))
-    for i in idxs:
-        if i >= 1:
-            break
-        elem = dset[i]
+    _, ax = plt.subplots(1, 5, figsize=(16, 8))
 
-        df_pred = elem["predicted_distance_field"]
-        df = elem["deeplsd_distance_field"]
+    df_pred = elem["predicted_distance_field"]
+    df_pred2 = elem["predicted_distance_field2"]
+    df = elem["deeplsd_distance_field"]
 
-        ax[0].axis('off')
-        ax[0].set_title('Ground Distance Field')
-        ax[0].imshow(df)
+    ax[0].axis('off')
+    ax[0].set_title('Ground Distance Field')
+    ax[0].imshow(df)
 
-        ax[1].axis('off')
-        ax[1].set_title('Predicted Distance Field')
-        ax[1].imshow(df_pred)
+    ax[1].axis('off')
+    ax[1].set_title('Predicted Distance Field')
+    ax[1].imshow(df_pred)
 
-        ax[2].axis('off')
-        ax[2].set_title('Original')
-        ax[2].imshow(elem["image"].permute(1,2,0))
+    ax[2].axis('off')
+    ax[2].set_title('Predicted Distance Field 100k')
+    ax[2].imshow(df_pred2)
+
+    image = elem["image"].permute(1,2,0).cpu().numpy()
+    image = np.ascontiguousarray((255*image).astype(np.uint8))
+    for p in elem["lines"][0]:
+        p = p.cpu().numpy().astype(int)
+        cv2.line(image, 
+                     (p[0,0], p[0,1]), 
+                     (p[1,0], p[1,1]), 
+                     color=(0, 255, 0), 
+                     thickness=2)
+    ax[3].axis('off')
+    ax[3].set_title('Base lines')
+    ax[3].imshow(image)
+
+    image = elem["image"].permute(1,2,0).cpu().numpy()
+    image = np.ascontiguousarray((255*image).astype(np.uint8))
+    for p in elem["lines2"][0]:
+        p = p.cpu().numpy().astype(int)
+        cv2.line(image, 
+                     (p[0,0], p[0,1]), 
+                     (p[1,0], p[1,1]), 
+                     color=(0, 255, 0), 
+                     thickness=2)
+    ax[4].axis('off')
+    ax[4].set_title('100k lines')
+    ax[4].imshow(image)
 
     plt.savefig(f"test_{name}.png")
-    plt.show()
     plt.close()
 
 # Command to launch it
-# python -m gluefactory.test_oxparis_100k_output --conf gluefactory/configs/train_jpl_oxparis_100k.yaml 
+# python -m gluefactory.test_oxparis_100k_output --conf gluefactory/configs/benchmark_jpl_lsd.yaml 
 # python -m gluefactory.test_oxparis_100k_output --conf gluefactory/configs/train_jpl_oxparis_base.yaml 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -103,8 +97,11 @@ if __name__ == "__main__":
     print("Train dataset loaded")
 
     print("Loading model")
-    jpldd_model = load_model(conf.model, "/cluster/scratch/fcosta/outputs/training/TEST_TRAIN/checkpoint_best.tar").to("cpu")
+    jpldd_model = load_model(conf.model.extractor, "/home/francois/Bureau/glue-factory/outputs/training/TEST_TRAIN").to("cpu")
     jpldd_model.eval()
+
+    jpldd_model_2 = load_model(conf.model.extractor, "/home/francois/Bureau/glue-factory/outputs/training/TRAIN_100k_DISTRIBUTED_4").to("cpu")
+    jpldd_model_2.eval()
     print("Model loaded")
 
     for it, data in enumerate(train_loader):
@@ -112,17 +109,22 @@ if __name__ == "__main__":
         dataset = {}
 
         output_jpldd = jpldd_model({"image": data["image"].to("cpu").unsqueeze(0)})
-    
+        output_jpldd2 = jpldd_model_2({"image": data["image"].to("cpu").unsqueeze(0)})
+
         # Load image
         dataset["image"] = data["image"]
+        dataset["lines"] = output_jpldd["lines"]
+        dataset["lines2"] = output_jpldd2["lines"]
 
         #  Predicted distance field
         img_np = output_jpldd["line_distancefield"].detach().numpy()[0]
-        img_np = nicer_display(img_np) * 255
-        dataset["predicted_distance_field"] = -img_np
+        dataset["predicted_distance_field"] = img_np
+
+        img_np = output_jpldd2["line_distancefield"].detach().numpy()[0]
+        dataset["predicted_distance_field2"] = img_np
 
         img_np = data["deeplsd_distance_field"].numpy()
-        img_np = nicer_display(img_np) * 255
-        dataset["deeplsd_distance_field"] = -img_np
 
-        visualize_img_with_gt(it, [dataset])
+        dataset["deeplsd_distance_field"] = img_np
+
+        visualize_img_with_gt(it, dataset)
