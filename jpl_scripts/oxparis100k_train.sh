6d54605f98b789139344770ea600aec7658306aa
François Costa <frankost.costa@gmail.com>
2025-07-31 16:58:10 +0200

    Integrate VIT backbone with JPL

--- jpl_scripts/oxparis100k_train.sh
+++ jpl_scripts/oxparis100k_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Cmd params 'run_training_euler.sh [exp_name] [path to conf]'
  
-#SBATCH --time=2-00:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --gpus=4
+#SBATCH --gpus=8
 #SBATCH --gres=gpumem:23g
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=6000
@@ -15,4 +15,4 @@ source $DIR/common.sh
 SetupStack
 
 echo "Starting training"
-python -m gluefactory.train TRAIN_100k --conf=gluefactory/configs/train_jpl_oxparis_100k.yaml --distributed
+python -m gluefactory.train TRAIN_100k_VIT_ALL_PIXELS_5 --conf=gluefactory/configs/train_jpl_oxparis_100k_vit_5_threshold.yaml --distributed
