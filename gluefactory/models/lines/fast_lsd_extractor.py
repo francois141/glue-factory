9ece76f1008434586c4416450b72aa404cf0ddbf
François Costa <frankost.costa@gmail.com>
2025-08-15 14:54:24 +0200

    dumping code

--- gluefactory/models/lines/fast_lsd_extractor.py
+++ gluefactory/models/lines/fast_lsd_extractor.py
@@ -3,12 +3,11 @@ from typing import Optional
 import numpy as np
 import torch
 from faster_pytlsd import lsd as fast_lsd
-from pytlsd import lsd, lsd_from_points
+from pytlsd import lsd
 
 from gluefactory.models.lines.line_refinement import filter_outlier_lines, merge_lines
 from gluefactory.models.lines.line_utils import preprocess_angle
 from gluefactory.utils.image import compute_image_grad
-import torchvision.transforms as T
 
 from ..base_model import BaseModel
 
@@ -31,9 +30,6 @@ class FastLSDLineExtractor(BaseModel):
         "faster_lsd": True,
         "return_line_descriptors": False,
         "trainable": False,
-        "sigma": 0.6,
-        "threshold_value": 5.2262518595055063,
-        "kernel_size": 7,
     }
     required_data_keys = ["image"]
 
@@ -47,63 +43,10 @@ class FastLSDLineExtractor(BaseModel):
             raise NotImplementedError(
                 "Line descriptors are not implemented yet for FasterLSD"
             )
-        
-    def compute_gradient_2d_noborder(self, in_tensor: torch.Tensor) -> torch.Tensor:
-
-        # GaussianBlur: kernel size must be odd and positive
-        blur = T.GaussianBlur(kernel_size=self.conf.kernel_size, sigma=self.conf.sigma)
-
-        # Apply blur
-        in_tensor = blur(in_tensor.unsqueeze(0))[0]
-
-        H, W = in_tensor.shape
-        com1 = in_tensor[1:-1, 1:-1] - in_tensor[:-2, :-2]
-        com2 = in_tensor[:-2, 1:-1] - in_tensor[1:-1, :-2]
-
-        gx = com1 + com2
-        gy = com1 - com2
-        norm2 = gx * gx + gy * gy
-        norm = torch.sqrt(norm2 / 4.0)
-
-        out = torch.zeros_like(in_tensor)
-        out[1:-1, 1:-1] = norm
-
-        # Compute angle where norm > threshold
-        threshold = self.conf.threshold_value
-        mask = norm > threshold
-
-        # atan2(-gx, gy) for pixels where norm > threshold
-        angle_vals = torch.atan2(-gx[mask], gy[mask])
-
-        # Insert angles back in output angle tensor
-        out_angle = torch.zeros_like(in_tensor).to(torch.float)
-        out_angle[1:-1, 1:-1][mask] = angle_vals
-
-        return out, out_angle
-
-
-    def extract_points(self, df: torch.Tensor):
-
-        # Find positions where df > 0
-        positions = (df > torch.quantile(df.to(torch.float), 0.75)).nonzero(as_tuple=False)  # shape [num_points, 2]
-
-        # Gather intensities at those positions
-        intensities = df[positions[:, 0], positions[:, 1]]
-
-        # Sort intensities in descending order
-        sorted_indices = torch.argsort(intensities, descending=True)
-        positions_sorted = positions[sorted_indices]
-
-        # Swap columns to get [x, y] instead of [row, col] and flatten
-        keypoints_importants = positions_sorted[:, [1, 0]].contiguous()
-
-        # Limit to first 100000 keypoints
-        return keypoints_importants[:100000]
-
 
     def detect_lines(
-        self, img, df, line_level = None
-    ):
+        self, img: np.array, df: np.array, line_level: Optional[np.array] = None
+    ) -> np.array:
         """
         detect lines in one image.
         Args:
@@ -114,69 +57,79 @@ class FastLSDLineExtractor(BaseModel):
         """
         # Run LSD
         img_grad_angle = None
-        gradnorm = torch.clamp(5.0 - df, min=0.0).to(torch.float64)
+        gradnorm = np.maximum(5 - df, 0).astype(np.float64)
 
-
-        with_points = True
-
-        if with_points:
-            gradient, angle = self.compute_gradient_2d_noborder(img)
-            interests_points = self.extract_points(gradient)
-            lines = lsd_from_points(
-                img.cpu().numpy().astype(np.float64), 
-                interests_points.cpu().numpy().astype(np.float64),
-                scale=1.0,         
-                gradnorm=gradnorm.cpu().numpy(), 
-                gradangle=angle.cpu().numpy(), 
-                grad_nfa=False
-            )[:, :4].reshape(-1, 2, 2)
-        elif self.conf.faster_lsd and False:
+        if self.conf.use_img_grad_angle:
+            img_grad_angle = compute_image_grad(img)[3]
+            angle = np.mod(img_grad_angle - np.pi / 2, 2 * np.pi)
+        else:
+            angle = line_level.astype(np.float64) - np.pi / 2
+            angle = preprocess_angle(angle, img, mask=True)[0]
+        angle[gradnorm < self.conf.grad_thresh] = -1024
+        if self.conf.faster_lsd:
             lines = fast_lsd(
-                img.cpu().numpy().astype(np.float64),
+                img.astype(np.float64),
                 scale=1.0,
-                gradnorm=gradnorm.cpu().numpy(),
+                gradnorm=gradnorm,
+                gradangle=angle,
                 grad_nfa=self.conf.grad_nfa,
             )[:, :4].reshape(-1, 2, 2)
         else:
             lines = lsd(
-                img.cpu().numpy().astype(np.float64),
+                img.astype(np.float64),
                 scale=1.0,
-                gradnorm=gradnorm.cpu().numpy(),
-                grad_nfa=False,
+                gradnorm=gradnorm,
+                gradangle=angle,
+                grad_nfa=self.conf.grad_nfa,
             )[:, :4].reshape(-1, 2, 2)
-
-
-        lines = torch.tensor(lines)
-
+        # Optionally filter out lines based on the DF and line_level
+        if self.conf.filtering is not None:
+            if self.conf.filtering == "strict":
+                df_thresh, ang_thresh = 1.0, np.pi / 12
+            else:
+                df_thresh, ang_thresh = 1.5, np.pi / 9
+            if self.conf.use_img_grad_angle:
+                angle = img_grad_angle
+            else:
+                angle = line_level - np.pi / 2
+            lines = filter_outlier_lines(
+                img,
+                lines[:, :, [1, 0]],
+                df,
+                angle,
+                mode="inlier_thresh",
+                use_grad=False,
+                inlier_thresh=0.5,
+                df_thresh=df_thresh,
+                ang_thresh=ang_thresh,
+            )[0][:, :, [1, 0]]
         # Now perform optional min length filtering and apply force num lines if needed
-        lengths = torch.norm(lines[:, 0] - lines[:, 1], dim=1)
+        lengths = np.linalg.norm(lines[:, 0] - lines[:, 1], axis=1)
         to_keep = lengths >= self.conf.min_length
         lines, lengths = lines[to_keep], lengths[to_keep]
 
         # Keep the best lines (best lines are the shortest ones)
-        scores = torch.sqrt(lengths)
+        scores = np.sqrt(lengths)
         lines = lines[:, :4].reshape(-1, 2, 2)
-        indices = torch.argsort(-scores)
+        indices = np.argsort(-scores)
         if self.conf.max_num_lines is not None:
             indices = indices[: self.conf.max_num_lines]
             lines = lines[indices]
 
         if self.conf.merge:
             lines = merge_lines(
-                lines, thresh=4, overlap_thresh=0
-            )
+                torch.from_numpy(lines), thresh=4, overlap_thresh=0
+            ).numpy()
 
         # Pad if necessary
         n = len(lines)
-        valid_mask = torch.ones(n, dtype=bool, device=lines.device)
+        valid_mask = np.ones(n, dtype=bool)
         if self.conf.force_num_lines:
             pad = self.conf.max_num_lines - n
-            if pad > 0:
-                pad_lines = torch.zeros((pad, 2, 2), dtype=lines.dtype, device=lines.device)
-                lines = torch.cat([lines, pad_lines], dim=0)
-
-                pad_mask = torch.zeros(pad, dtype=torch.bool, device=lines.device)
-                valid_mask = torch.cat([valid_mask, pad_mask], dim=0)
+            lines = np.concatenate(
+                [lines, np.zeros((pad, 2, 2), dtype=np.float32)], axis=0
+            )
+            valid_mask = np.concatenate([valid_mask, np.zeros(pad, dtype=bool)], axis=0)
 
         return {"lines": lines, "valid_lines": valid_mask}
 
@@ -193,16 +146,17 @@ class FastLSDLineExtractor(BaseModel):
         line_df_denormalized = data["line_distancefield"]
 
         # preprocess input to lsd
-        image = (image[:, 0] * 255).to(torch.uint8)
+        np_img = (image.cpu().numpy()[:, 0] * 255).astype(np.uint8)
+        np_df = line_df_denormalized.cpu().numpy()
+        np_ll = line_level.cpu().numpy()
         lines = []
-
         # valid lines contain a line mask indicating which lines were actually predicted and which are padding
         # applied to enable batching via conf.force_num_lines
         valid_lines = []
-        for img, df, ll in zip(image, line_df_denormalized, line_level):
-            line_pred = self.detect_lines(img, df, line_level)
-            lines.append(line_pred["lines"])
-            valid_lines.append(line_pred["valid_lines"])
+        for img, df, ll in zip(np_img, np_df, np_ll):
+            line_pred = self.detect_lines(img, df, ll)
+            lines.append(torch.Tensor(line_pred["lines"]))
+            valid_lines.append(torch.Tensor(line_pred["valid_lines"]))
         # Here a list of lines for each img is returned.
         outputs = {"lines": lines, "valid_lines": valid_lines}
         return outputs
