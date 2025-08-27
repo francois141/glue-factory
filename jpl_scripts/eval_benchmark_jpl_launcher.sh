0311c7dd847b02af901c21ea08ad659588292bea
Francois Costa <frankost.costa@gmail.com>
2025-08-27 09:42:32 +0200

    Removing old unused scripts

--- jpl_scripts/eval_benchmark_jpl_launcher.sh
+++
@@ -1,6 +0,0 @@
-TRAIN_CONFIGURATIONS=("TEST_TRAIN" "TEST_TRAIN_DISTRIBUTED")
-
-for configuration in "${TRAIN_CONFIGURATIONS[@]}"; do
-  echo "Benchmarking current training run: $configuration"
-  sbatch ./jpl_scripts/eval_benchmark_jpl.sh $configuration
-done
