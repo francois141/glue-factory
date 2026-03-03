bf73ac29018d47b736b19b86acb7418a9ac8f505
rkreft <rkreft@ethz.ch>
2026-03-03 12:51:53 +0100

    mask gpu in cpu measurement for timing

--- jpl_scripts/eval_performance.sh
+++ jpl_scripts/eval_performance.sh
@@ -58,5 +58,5 @@ for config_entry in "${CONFIGS[@]}"; do
     IFS='|' read -r display_name config_path output_file <<< "$config_entry"
     echo "$display_name"
     python -m gluefactory.eval.timing_measurement --conf="$config_path" --num_s=$NUMER_RUNS_GPU --device=cuda > "${OUTPUT_DIR}/${output_file}_gpu"
-    python -m gluefactory.eval.timing_measurement --conf="$config_path" --num_s=$NUMER_RUNS_CPU --device=cpu > "${OUTPUT_DIR}/${output_file}_cpu"
+    CUDA_VISIBLE_DEVICES="" python -m gluefactory.eval.timing_measurement --conf="$config_path" --num_s=$NUMER_RUNS_CPU --device=cpu > "${OUTPUT_DIR}/${output_file}_cpu"
 done
